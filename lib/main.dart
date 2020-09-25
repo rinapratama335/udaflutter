ae222023096928f85a40331566c442c4c4195cd1
irwantowibowotech <irwantowibowotech@gmail.com>
2020-09-25 10:39:52 +0700

    membuat switch widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,37 +10,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double drag = 1.0;
-
-  void method1(value) {
-    setState(() {
-      drag = value;
-    });
-  }
+  bool sbool = false;
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Slider Widget',
+    return MaterialApp(
+      title: 'Switch Widget',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Slider Widget'),
-          backgroundColor: Colors.lightBlue,
+          title: new Text('Switch Widget'),
+          backgroundColor: Colors.green,
         ),
         body: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Slider(
-                  value: drag,
-                  min: 1.0,
-                  max: 10.0,
-                  onChanged: (double val) {
-                    method1(val);
-                  }
-              ),
-              new Text('value: $drag'),
-            ],
+          child: new Switch(
+              value: sbool,
+              onChanged: (bool sb) {
+                setState(() {
+                  sbool = sb;
+                  print(sbool);
+                });
+              }
           ),
         ),
       ),
