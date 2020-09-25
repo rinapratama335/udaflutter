f2ef3341d591838570a1f9bf64184df208b0bc73
Irwanto Wibowo <42676690+rinapratama335@users.noreply.github.com>
2020-09-25 11:13:25 +0700

    membuat drawer widget dengan navigasi dasar (#15)
    Co-authored-by: irwantowibowotech <irwantowibowotech@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'halamanlain.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new MaterialApp(home: MyApp(),));
 }
 
 class MyApp extends StatefulWidget {
@@ -10,27 +11,53 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool sbool = false;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Switch Widget',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Switch Widget'),
-          backgroundColor: Colors.green,
-        ),
-        body: new Center(
-          child: new Switch(
-              value: sbool,
-              onChanged: (bool sb) {
-                setState(() {
-                  sbool = sb;
-                  print(sbool);
-                });
-              }
-          ),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Navigasi Drawer'),
+        backgroundColor: Colors.green,
+      ),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+                accountName: Text('Irwanto'),
+                accountEmail: Text('irwanto@yahoo.com'),
+                currentAccountPicture: new CircleAvatar(
+                  backgroundColor: Colors.black26,
+                  child: new Text('P'),
+                ),
+                decoration: new BoxDecoration(
+                  color: Colors.lightBlue,
+                ),
+                otherAccountsPictures: <Widget>[
+                  new CircleAvatar(
+                    backgroundColor: Colors.black26,
+                    child: new Text('Y'),
+                  ),
+                  new CircleAvatar(
+                    backgroundColor: Colors.black26,
+                    child: new Text('W'),
+                  )
+                ]
+            ),
+            new ListTile(
+              title: new Text('Home Page'),
+              trailing: new Icon(Icons.home),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext contex) => new pagebaru('Welcome to Home Page'))),
+            ),
+            new ListTile(
+              title: new Text('List Laptop'),
+              trailing: new Icon(Icons.laptop),
+              onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new pagebaru('Welcome to List Product'))),
+            ),
+            new ListTile(
+              title: new Text('Close'),
+              trailing: new Icon(Icons.close),
+              onTap: (){Navigator.pop(context);},
+            ),
+          ],
         ),
       ),
     );
